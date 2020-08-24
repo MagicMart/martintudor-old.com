fe30b09edf4a21377dd37d22e44408fcd2f0c401
Martin Tudor <martcan27@gmail.com>
2020-08-24 16:12:56 +0100

    update header

--- src/components/App.js
+++ src/components/App.js
@@ -6,13 +6,39 @@ const Page = styled.div`
     max-width: 960px;
     height: 100%;
     background: whitesmoke;
-    margin: 0 auto;
+    margin: 10px auto;
+    padding: 10px;
 `;
+
+const StyledHeader = styled.header`
+    display: flex;
+    .subtitle {
+        display: block;
+        font-size: 1.4rem;
+        font-weight: 700;
+        color: #d15c21;
+        text-align: center;
+    }
+`;
+
+function Header() {
+    return (
+        <StyledHeader>
+            <div>
+                <h1 className="title">
+                    Martin Tudor
+                    <span className="subtitle">Web Developer</span>
+                </h1>
+            </div>
+        </StyledHeader>
+    );
+}
 
 function App() {
     return (
         <Page>
-            <h1>Hello World</h1>
+            <Header />
+            <hr />
         </Page>
     );
 }
