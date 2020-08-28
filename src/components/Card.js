344bc2ebad6451aee02ad78684c0fc47a8dadd5c
Martin Tudor <martcan27@gmail.com>
2020-08-28 16:52:33 +0100

    update styles

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,24 +3,24 @@ import React from "react";
 import styled from "styled-components";
 
 const StyledCard = styled.li`
-    flex: 0 1 250px;
+    flex: 0 1 200px;
     border: 1px solid green;
     margin: 5px;
     box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
     transition: 0.3s;
-    :hover {
-        box-shadow: 0 8px 16px 0 rgba(0, 0, 0, 0.2);
+    &:hover {
+        /* box-shadow: 0 8px 16px 0 rgba(0, 0, 0, 0.2); */
+        border: 3px solid orange;
     }
     img {
         width: 100%;
         border-bottom: 1px solid green;
     }
-    .container {
-        padding: 2px 8px;
-    }
-    h4 {
-        margin: 0;
+
+    .title {
         text-align: center;
+        margin: 0;
+        padding: 1em 0;
     }
 `;
 
@@ -36,9 +36,8 @@ function Card({ title, img, alt, link }: CardProps) {
         <StyledCard>
             <a href={link}>
                 <img src={img} alt={alt} />
-                <div className="container">
-                    <h4>{title}</h4>
-                </div>
+
+                <p className="title">{title}</p>
             </a>
         </StyledCard>
     );
