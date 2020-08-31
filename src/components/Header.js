afffc2bec18fc8e50fa769b3681d8fd6e194a49b
Martin Tudor <martcan27@gmail.com>
2020-08-31 19:35:21 +0100

    github link

--- src/components/Header.js
+++ src/components/Header.js
@@ -16,6 +16,10 @@ const StyledHeader = styled.header`
         font-size: 1.5rem;
         margin: 0 auto;
     }
+
+    a {
+        text-decoration: none;
+    }
     .subtitle {
         box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
         display: block;
@@ -23,6 +27,7 @@ const StyledHeader = styled.header`
         font-weight: 700;
         color: #d15c21;
         text-align: center;
+        padding: 4px;
     }
 `;
 
@@ -33,7 +38,9 @@ function Header() {
                 <img src={logo} alt="logo" />
                 <h1 className="title">
                     Martin Tudor
-                    <span className="subtitle">Web Developer</span>
+                    <a href="https://github.com/MagicMart">
+                        <span className="subtitle">Web Developer</span>
+                    </a>
                 </h1>
             </div>
         </StyledHeader>
