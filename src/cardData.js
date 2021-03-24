ad5d74983dd62829bd40f0edbe764ba9e6429779
Martin Tudor <martcan27@gmail.com>
2021-03-24 15:39:11 +0000

    update alt text

--- src/cardData.js
+++ src/cardData.js
@@ -25,21 +25,21 @@ const cardData = [
     {
         img:
             "https://marts-portfolio.s3.eu-west-2.amazonaws.com/images/weather-app.jpg",
-        alt: "Still from weather app",
+        alt: "A weather app",
         link: "https://martin-weather-app.netlify.com/",
         title: "Weather App",
     },
     {
         img:
             "https://marts-portfolio.s3.eu-west-2.amazonaws.com/images/hacker-news.jpg",
-        alt: "Still showing posts from hacker news",
+        alt: "Posts from hacker news",
         link: "https://marts-hacker-news.netlify.com/",
         title: "Hacker News",
     },
     {
         img:
             "https://marts-portfolio.s3.eu-west-2.amazonaws.com/images/game-of-life.jpg",
-        alt: "A stillimg from The Game Of life app",
+        alt: "The Game Of life app",
         link: "https://magicmart.github.io/Game-of-Life/",
         title: "Game of Life",
     },
