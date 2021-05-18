93edfe6ba97f3ba0828c666ae889a7ccc409300f
Martin Tudor <martcan27@gmail.com>
2021-05-18 16:45:15 +0100

    update projects

--- src/cardData.js
+++ src/cardData.js
@@ -1,44 +1,38 @@
 // @flow
 const cardData = [
     {
-        img:
-            "https://marts-portfolio.s3.eu-west-2.amazonaws.com/images/memory-cards.jpg",
+        img: "https://marts-portfolio.s3.eu-west-2.amazonaws.com/images/memory-cards.jpg",
         alt: "Memory game cards - with React.js",
         link: "https://memory.martintudor.com/",
         title: "Memory Game Cards",
     },
 
     {
-        img:
-            "https://marts-portfolio.s3.eu-west-2.amazonaws.com/images/martsphotos-500x500.jpg",
+        img: "https://marts-portfolio.s3.eu-west-2.amazonaws.com/images/martsphotos-500x500.jpg",
         alt: "A photo portfolio website",
         link: "https://photos.martintudor.com/",
         title: "Mart's Photos",
     },
     {
-        img:
-            "https://marts-portfolio.s3.eu-west-2.amazonaws.com/images/simple-blogger.jpg",
-        alt: "Simple Blogger front page",
-        link: "https://simple-blogger.netlify.com/",
-        title: "Simple Blogger",
+        img: "https://marts-portfolio.s3.eu-west-2.amazonaws.com/images/update-menu.jpg",
+        alt: "Update Menu",
+        link: "https://audio.martintudor.com/",
+        title: "Update Audio",
     },
     {
-        img:
-            "https://marts-portfolio.s3.eu-west-2.amazonaws.com/images/weather-app.jpg",
+        img: "https://marts-portfolio.s3.eu-west-2.amazonaws.com/images/weather-app.jpg",
         alt: "A weather app",
         link: "https://martin-weather-app.netlify.com/",
         title: "Weather App",
     },
     {
-        img:
-            "https://marts-portfolio.s3.eu-west-2.amazonaws.com/images/hacker-news.jpg",
+        img: "https://marts-portfolio.s3.eu-west-2.amazonaws.com/images/hacker-news.jpg",
         alt: "Posts from hacker news",
         link: "https://hacker.martintudor.com/",
         title: "Hacker News",
     },
     {
-        img:
-            "https://marts-portfolio.s3.eu-west-2.amazonaws.com/images/game-of-life.jpg",
+        img: "https://marts-portfolio.s3.eu-west-2.amazonaws.com/images/game-of-life.jpg",
         alt: "The Game Of life app",
         link: "https://life.martintudor.com/",
         title: "Game of Life",
